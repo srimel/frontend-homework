0a183ca2ff237f181d64333c0ffbf09bdfd711c4
Stuart Rimel <stuart.rimel@gmail.com>
2023-04-09 15:56:05 -0700

    Add logic for palindrome detection

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,22 @@
-const elem = document.querySelector('input');
+const input = document.querySelector('input');
+const result = document.getElementById("result");
 
-elem.addEventListener('input', handleInput);
+input.addEventListener('input', handleInput);
+
+function handleInput(e) {
+    const inputNumberStr = e.target.value;
+    if (inputNumberStr.length < 2) {
+        result.textContent = "";
+    } else if (isPalindrome(inputNumberStr)) {
+        result.style.color = "green"
+        result.textContent = "Yes. This is a palindrome!";
+    } else {
+        result.style.color = "red"
+        result.textContent = "No. Try again.";
+    }
+}
+
+function isPalindrome(inputNumberStr) {
+    const reverseNumberStr = inputNumberStr.split('').reverse().join('');
+    return inputNumberStr === reverseNumberStr;
+}
