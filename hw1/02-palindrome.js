af18e8f7a5d1cb22b3afb06ae8746c7dd29f15a9
Stuart Rimel <stuart.rimel@gmail.com>
2023-04-11 22:49:24 -0700

    Refactor functions to conform to AirBnb style guide

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,29 +1,29 @@
 const input = document.querySelector('input');
 const result = document.getElementById('result');
 
-input.addEventListener('input', handleInput);
+const isBadInput = function checkIfBadInput(inputNumberStr) {
+  return !/^[0-9.]*$/.test(inputNumberStr);
+};
 
-function handleInput(e) {
+const isPalindrome = function checkIfPalindrome(inputNumberStr) {
+  const reverseNumberStr = inputNumberStr.split('').reverse().join('');
+  return inputNumberStr === reverseNumberStr;
+};
+
+const handleInput = function handleInputEvent(e) {
   const inputNumberStr = e.target.value;
   if (inputNumberStr.length < 1) {
     result.textContent = '';
   } else if (isBadInput(inputNumberStr)) {
-    result.style.color = 'red';
+    result.className = 'card-text text-danger';
     result.textContent = 'Invalid Input Error: Only positive numbers allowed.';
   } else if (isPalindrome(inputNumberStr)) {
-    result.style.color = 'green';
+    result.className = 'card-text text-success';
     result.textContent = 'Yes. This is a palindrome!';
   } else {
-    result.style.color = 'red';
+    result.className = 'card-text text-danger';
     result.textContent = 'No. Try again.';
   }
-}
+};
 
-function isPalindrome(inputNumberStr) {
-  const reverseNumberStr = inputNumberStr.split('').reverse().join('');
-  return inputNumberStr === reverseNumberStr;
-}
-
-function isBadInput(inputNumberStr) {
-  return !/^[0-9.]*$/.test(inputNumberStr);
-}
+input.addEventListener('input', handleInput);
