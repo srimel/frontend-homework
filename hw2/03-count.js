0d8522232bc92134fecfb469203427c65fb9a73b
Stuart Rimel <stuart.rimel@gmail.com>
2023-04-16 04:23:27 -0700

    WIP count

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,34 @@
-// Add your code here
+// Stuart Rimel
 
-input.addEventListener('keydown', handleKeyDown);
+const inputElement = document.getElementById('search-input');
+const textElement = document.getElementById('text');
+const originalInnerHTML = textElement.innerHTML;
+
+const highlight = function highlight(word, searchText) {
+  return word.replace(
+    searchText,
+    `<span style="background-color: yellow">${searchText}</span>`
+  );
+};
+
+const findAndHighlight = function findAndHighlight(searchText) {
+  let words = originalInnerHTML.split(' ');
+  for (i = 0; i < words.length; i++) {
+    if (words[i].includes(searchText)) {
+      words[i] = highlight(words[i], searchText);
+    }
+  }
+  textElement.innerHTML = words.join(' ');
+};
+
+const handleKeyDown = function handleKeyDown(event) {
+  let searchText = event.target.value;
+  if (event.key.length === 1) {
+    searchText += event.key;
+  } else if (event.key === 'Backspace') {
+    searchText = searchText.slice(0, -1);
+  }
+  findAndHighlight(searchText);
+};
+
+inputElement.addEventListener('keydown', handleKeyDown);
