b7ca6646438926078369f14e6d19e0aa0f81beea
Stuart Rimel <stuart.rimel@gmail.com>
2023-04-18 01:31:32 -0700

    WIP form submission

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,26 @@
-// Add your code here
+// Stuart Rimel
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const name = event.target[0].value;
+  const email = event.target[1].value;
+  const status = event.target[2].value;
+  const programmingLang = event.target[4].checked;
+  const operatingSystems = event.target[5].checked;
+  const fullStack = event.target[6].checked;
+  const textArea = event.target[7].value;
+  console.log(`FORM SUBMITTED!
+    Details of submission:
+        name: ${name}
+        email: ${email}
+        registration status: ${status}
+        previous classes:
+            Programming Languages: ${programmingLang}
+            Operating Systems: ${operatingSystems}
+            Full Stack Web Development: ${fullStack}
+    Additional Comments: 
+        ${textArea}
+  `);
+  form.reset();
+});
