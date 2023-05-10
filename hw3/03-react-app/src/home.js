7504ec2efa67b0ada65e8a7145515b5abcfbd60e
Stuart Rimel <stuart.rimel@gmail.com>
2023-05-10 01:11:11 -0700

    WIP dougnut

--- hw3/03-react-app/src/home.js
+++ hw3/03-react-app/src/home.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="welcome-msg">
       <h1>Hello, Welcome!</h1>
-      <p>Make yourself right at home!</p>
+      <p>This is a website for Game of Thrones fans.</p>
     </div>
   );
 }
--- hw3/03-react-app/src/houses.js
+++ hw3/03-react-app/src/houses.js
@@ -1,3 +1,121 @@
+import { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import { Chart, DoughnutController, ArcElement } from 'chart.js';
+
+const backgroundColors = [
+  'rgba(54, 162, 235, 0.8)',
+  'rgba(255, 206, 86, 0.8)',
+  'rgba(255, 99, 132, 0.8)',
+  'rgba(75, 192, 192, 0.8)',
+  'rgba(153, 102, 255, 0.8)',
+  'rgba(255, 159, 64, 0.8)',
+  'rgba(199, 199, 199, 0.8)',
+  'rgba(83, 102, 255, 0.8)',
+  'rgba(40, 159, 64, 0.8)',
+  'rgba(210, 199, 199, 0.8)',
+  'rgba(78, 52, 199, 0.8)',
+];
+
+const borderColors = [
+  'rgba(54, 162, 235, 1)',
+  'rgba(255, 206, 86, 1)',
+  'rgba(255, 99, 132, 1)',
+  'rgba(75, 192, 192, 1)',
+  'rgba(153, 102, 255, 1)',
+  'rgba(255, 159, 64, 1)',
+  'rgba(159, 159, 159, 1)',
+  'rgba(83, 102, 255, 1)',
+  'rgba(40, 159, 64, 1)',
+  'rgba(210, 199, 199, 1)',
+  'rgba(78, 52, 199, 1)',
+];
+
+const url = 'https://thronesapi.com/api/v2/Characters';
+
+const getColors = (dataArray, colorArray) => {
+  const repeatedColors = [];
+  dataArray.forEach((item, index) => {
+    repeatedColors.push(colorArray[index % colorArray.length]);
+  });
+  return repeatedColors;
+};
+
 export default function Houses() {
-  return <></>;
+  const [chartData, setChartData] = useState(null);
+  const donutChartRef = useRef(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        const data = response.data;
+        handleResponseData(data);
+      } catch (error) {
+        console.error('Request failed', error);
+      }
+    };
+
+    fetchData();
+  });
+
+  const handleResponseData = function handleResponseData(data) {
+    console.log('Response Data:', data);
+    const { houseNames, houseCounts } = getHouseNameCounts(data);
+    renderChart(houseNames, houseCounts);
+  };
+
+  const getHouseNameCounts = function getHouseNameCountsFromData(data) {
+    const familyCounts = {};
+    data.forEach((obj) => {
+      let { family } = obj;
+      family = family || obj.lastName;
+      familyCounts[family] = familyCounts[family]
+        ? familyCounts[family] + 1
+        : 1;
+    });
+    const houseNames = Object.keys(familyCounts);
+    const houseCounts = houseNames.map((house) => familyCounts[house]);
+    return {
+      houseNames,
+      houseCounts,
+    };
+  };
+
+  let chartInstance = null;
+
+  // FIXME
+  const renderChart = function renderTheChart(houses, counts) {
+    const donutChart = donutChartRef.current.getContext('2d');
+
+    Chart.register(DoughnutController, ArcElement);
+
+    if (chartInstance) {
+      chartInstance.destroy();
+    }
+
+    chartInstance = new Chart(donutChart, {
+      type: 'doughnut',
+      data: {
+        labels: houses,
+        datasets: [
+          {
+            label: 'My First Dataset',
+            data: counts,
+            backgroundColor: getColors(houses, backgroundColors),
+            borderColor: getColors(houses, borderColors),
+            borderWidth: 1,
+          },
+        ],
+      },
+    });
+  };
+
+  return (
+    <div>
+      <h1>House Counts</h1>
+      <div>
+        <canvas ref={donutChartRef} className="donut-chart" />
+      </div>
+    </div>
+  );
 }
